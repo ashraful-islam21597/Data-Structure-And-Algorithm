94f91a249122321e100afc65022a3bdbffbf9eb3
ashraful-islam21597 <ashraful.97mdp@gmail.com>
2023-12-19 22:45:41 +0600

    get shortest path feature added in graph algorithm

--- Graph Algorithm/graph.py
+++ Graph Algorithm/graph.py
@@ -15,12 +15,31 @@ class Graph:
         if start not in self.graph_dict:
             return []
         paths=[]
+        #print("----",self.graph_dict[start])
         for node in self.graph_dict[start]:
+
             if node not in path:
                 new_path=self.get_path(node,end,path)
+
                 for p in new_path:
                     paths.append(p)
         return paths
+    def get_shortest_path(self,start,end,path=[]):
+        path = path + [start]
+        if start==end:
+            return path
+        if start not in self.graph_dict:
+            return None
+        shortest_path=None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp=self.get_shortest_path(node,end,path)
+                if sp:
+                    if shortest_path is None or len(sp)<len(shortest_path):
+                        shortest_path=sp
+        return shortest_path
+
+
 
 
 
@@ -39,3 +58,5 @@ if __name__ == '__main__':
     end="New York"
     r=route_graph.get_path(start,end)
     print(r)
+    s=route_graph.get_shortest_path(start,end)
+    print(s)
